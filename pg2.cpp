2a80c993554fbc19c55e19f12760bbc476d2d0f1
Nikhithashetty23 <160234547+Nikhithashetty23@users.noreply.github.com>
2024-10-01 09:30:25 +0530

    Update pg2.cpp

--- pg2.cpp
+++ pg2.cpp
@@ -20,8 +20,8 @@ class Person {
 };
 
 int main(){
-  Person p1("Dasai",27);
-  Person p2("Kunikida",28);
+  Person p1("manu",27);
+  Person p2("shanu",28);
   p1.print_info();
   p2.print_info();
   return 0;
