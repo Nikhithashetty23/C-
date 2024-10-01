0eaeb1036c387d4d4835cd3b33a4843ed6a1d566
Nikhithashetty23 <160234547+Nikhithashetty23@users.noreply.github.com>
2024-10-01 09:26:26 +0530

    Update pg1.cpp

--- pg1.cpp
+++ pg1.cpp
@@ -1,1 +1,27 @@
+// P1. Define a Person class like we did in the previous module. Only add name and age.
 
+
+#include <iostream>
+using namespace std;
+
+class Person {
+    // Create a constructor that takes a string as a parameter
+    // and sets the name attribute to the value of the parameter
+    public:
+        Person(string name, int age) {
+            this->name = name;
+            this->age = age;
+        }
+        void print_info() {
+            cout << "My name is: " << name << " my age is: " << age << endl;
+        }
+    private:
+        string name;
+        int age;
+};
+
+int main(){
+  Person p1("Kalkur",27);
+  p1.print_info();
+  return 0;
+}
